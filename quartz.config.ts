ef52bbbce26934f96b9152bcb46b4deafacc1893
faw <git.fawanvl@gmail.com>
2025-03-17 12:56:53 +0700

    change site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Faw's Notes",
+    pageTitle: "🌼 Faw's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
